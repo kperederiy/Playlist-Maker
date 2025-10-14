f2b879440d8e5f5b78fc3b05cee81aae5dfede90
kperederiy <kperederiy@yandex.ru>
2025-10-14 14:38:51 +0300

    ДЗ 9.2

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,13 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +16,60 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val buttonBack = findViewById<ImageView>(R.id.back)
-
         buttonBack.setOnClickListener {
             finish()
+        }
+
+        val themeSwitch = findViewById<SwitchMaterial>(R.id.themeSwitch)
+
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        val buttonShare = findViewById<LinearLayout>(R.id.share)
+        buttonShare.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.share_url)
+                )
+                type = "text/plain"
+            }
+            val chooser = Intent.createChooser(shareIntent, getString(R.string.share_text))
+            startActivity(chooser)
+        }
+
+        val buttonSupport = findViewById<LinearLayout>(R.id.support)
+        buttonSupport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(
+                    Intent.EXTRA_EMAIL,
+                    getString(R.string.email)
+                )
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    getString(R.string.subject_email)
+
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.text_email)
+                )
+            }
+            startActivity(intent)
+        }
+
+        val buttonUserAgreement = findViewById<LinearLayout>(R.id.user_agreement)
+        buttonUserAgreement.setOnClickListener {
+            val intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_url)))
+            startActivity(intent)
         }
 
     }
