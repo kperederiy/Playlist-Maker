b5435880ac67e5795e464a3a7c5b2bde2ec69910
kperederiy <kperederiy@yandex.ru>
2025-10-31 10:54:09 +0300

    Задача спринт №10

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -6,10 +6,11 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class TrackAdapter(
     val tracks: List<Track>
-): RecyclerView.Adapter<TrackHolder>() {
+) : RecyclerView.Adapter<TrackHolder>() {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -32,7 +33,7 @@ class TrackAdapter(
     }
 }
 
-class TrackHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class TrackHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackName: TextView = itemView.findViewById(R.id.trackName)
     private val artistName: TextView = itemView.findViewById(R.id.artistName)
     private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
@@ -41,10 +42,10 @@ class TrackHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         trackName.text = track.trackName
         artistName.text = track.artistName
         trackTime.text = track.trackTime
-        /*Glide.with(itemView.context)
+        Glide.with(itemView.context)
             .load(track.artworkUrl100)
             .placeholder(R.drawable.placeholder)
-            .into(artworkUrl100)*/
+            .into(artworkUrl100)
     }
 
 }
