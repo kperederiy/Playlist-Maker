b38891c45a84af984979b549d957a1a066360871
kperederiy <kperederiy@yandex.ru>
2026-01-17 13:51:05 +0300

    подключил MediaPlayer

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
