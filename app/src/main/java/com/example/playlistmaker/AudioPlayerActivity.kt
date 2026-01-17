6c948a9b9c26bb0b1f125eadb49d5a6c900b5c68
kperederiy <kperederiy@yandex.ru>
2026-01-17 16:12:31 +0300

    Реализация отображение текущего времени воспроизведения отрывка трека, используя Handler

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -7,7 +7,6 @@ import android.os.Looper
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
